2f49e5e6f98a8e169bc89702aed124de6b11bf86
LittleViewer <169719223+LittleViewer@users.noreply.github.com>
2026-08-03 19:24:31 +0200

    Add basic 'i_am_alive' sensor CLI and config
    Introduce a simple sensor CLI that reads TOML config and posts an "I'm alive" message to a Gotify server. Adds: config_sensor.toml (parameter schema), config_tool_class.py (TOML loader/helper), main.py (CLI that builds flags from config and triggers sensors), and sensor/sensor_i_am_alive.py (posts message via requests). Intended to provide a minimal alive-check notifier using configured server IP/port/password.

---
+++ config_tool_class.py
@@ -0,0 +1,20 @@
+#base source : https://github.com/LittleViewer/WeakSignalFinder/blob/main/libCore/config_tool_class.py in AGPLv3
+import tomllib
+
+class config_toml_tool:
+
+    def key_return(self, table,key, sub_table = None):
+        if self.utC_.is_string(table) != True or self.utC_.is_string(key) != True:
+            self.utC_.error_with_reason("An error occurred with the configuration file!")
+            return False
+        if sub_table == None:
+            return self.config[table][key]
+        else:
+            if self.utC_.is_string(sub_table) != True:
+                self.utC_.error_with_reason("An error occurred with the configuration file!")
+                return False
+            return self.config[table][sub_table][key]
+
+    def __init__(self, path = "config_weakSignalFinder.toml"):
+        handle = open(self.utC_.absolute_link(path),"rb")
+        self.config = tomllib.load(handle)
---
+++ main.py
@@ -0,0 +1,18 @@
+import config_tool_class
+import sensor.sensor_i_am_alive
+import argparse
+
+
+ctC_ = config_tool_class.config_toml_tool()
+
+address = f"http://{ctC_.key_return("parameter","ip","server")}:{ctC_.key_return("parameter","port","server")}/message"
+password = ctC_.key_return("parameter","password_app","server")
+parser = argparse.ArgumentParser()
+argument_run = ctC_.key_return("parameter","list_flag","flag")
+
+for one_argument in argument_run:
+    parser.add_argument(one_argument, action="store_true")
+args = parser.parse_args()
+
+if args.i_am_alive:
+    sensor.sensor_i_am_alive.sensor_i_am_alive().pipe_lauch_alive(address, password)

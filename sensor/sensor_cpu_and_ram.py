e3ea8c09492ed2052e57c1bb218d16926ec1e34f
LittleViewer <169719223+LittleViewer@users.noreply.github.com>
2026-08-03 20:50:24 +0200

    Update sensor_cpu_and_ram.py

--- sensor/sensor_cpu_and_ram.py
+++ sensor/sensor_cpu_and_ram.py
@@ -10,10 +10,12 @@ class sensor_cpu_ram:
         if self.ctC_.key_return("parameter","ram","alert_level") >= self.ram_usage_percent :
             title += "RAM"
             message += f"{self.ram_usage_percent}% RAM usage"
+            spS.patron_sensor.request(address, password,f"{title} !",f"{message}!")
         if self.ctC_.key_return("parameter","cpu","alert_level") >= self.cpu_usage:
             title += "CPU"
             message += f"{self.cpu_usage}% CPU usage"
-        spS.patron_sensor.request(address, password,f"{title} !",f"{message}!")
+            spS.patron_sensor.request(address, password,f"{title} !",f"{message}!")
+        
 
     def __init__(self):
         self.ctC_ = ctC.config_toml_tool()

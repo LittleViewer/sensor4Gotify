afeff0e2e1937bed57eeb53b96fa75c1d15580c2
LittleViewer <169719223+LittleViewer@users.noreply.github.com>
2026-08-03 19:58:52 +0200

    Update sensor_i_am_alive.py

--- sensor/sensor_i_am_alive.py
+++ sensor/sensor_i_am_alive.py
@@ -1,8 +1,14 @@
 import sensor.patron_sensor as spS
 
 class sensor_i_am_alive:
+    def get_uptime(self):
+        with open('/proc/uptime', 'r') as f:
+            second = float(f.readline().split()[0])
+            if second == 0:
+                second = 0.1
+            self.uptime_seconds = second
 
     def pipe_lauch_alive(self, address, password):
-        spS.patron_sensor.request(address, password,"I'm alive !","Dearest creator, this message is to let you know that I am still up and running!")
+        spS.patron_sensor.request(address, password,"I'm alive !",f"Dearest creator, this message is to let you know that I have been running for {(self.get_uptime()/60)/60} hours now!")
     def __init__(self):
         pass
